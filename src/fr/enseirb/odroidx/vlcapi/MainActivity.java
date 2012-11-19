adf2e43d67f534377edf2087ca60349e724f400b
Fabien Fleurey <fabien@fleurey.org>
2012-11-19 15:02:57 +0100

    Add test scenario

--- src/fr/enseirb/odroidx/vlcapi/MainActivity.java
+++ src/fr/enseirb/odroidx/vlcapi/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.enseirb.odroidx.vlcapi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		((Button) findViewById(R.id.test_button)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(), TestPlayerActivity.class);
+				i.putExtra("itemLocation", "http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys12/BigBuckBunny/MPDs/BigBuckBunnyNonSeg_2s_isoffmain_DIS_23009_1_v_2_1c2_2011_08_30.mpd");
+				i.putExtra("itemTitle", "Test Title");
+				i.putExtra("dontParse", false);
+				startActivity(i);
+			}
+		});
 	}
 }
